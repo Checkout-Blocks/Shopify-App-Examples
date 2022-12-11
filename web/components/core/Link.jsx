7780a34e9bc67db87992e2d801859138c178060d
Gil Greenberg <gilgreenberg@gmail.com>
2022-12-11 12:34:00 -0500

    Add Link component for Polaris

---
+++ web/components/core/Link.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+const PolarisLink = ({ url, external, children, ...rest }) => {
+    const IS_EXTERNAL_LINK_REGEX = /^(?:[a-z][a-z\d+.-]*:|\/\/)/;
+
+    if (external || IS_EXTERNAL_LINK_REGEX.test(url)) {
+        return (
+            <a {...rest} href={url} target="_blank" rel="noopener noreferrer">
+                {children}
+            </a>
+        );
+    }
+
+    return (
+        <Link {...rest} href={url} as={url}>
+            {children}
+        </Link>
+    );
+};
+
+export default PolarisLink;
--- web/pages/_app.jsx
+++ web/pages/_app.jsx
@@ -2,10 +2,13 @@ import { AppProvider as PolarisProvider } from "@shopify/polaris";
 import polarisTranslations from "@shopify/polaris/locales/en.json";
 import "@shopify/polaris/build/esm/styles.css";
 
+import { Link } from "@components/core";
+
 function MyApp({ Component, pageProps }) {
     return (
         <PolarisProvider
             i18n={polarisTranslations}
+            linkComponent={Link}
         >
             <Component {...pageProps} />
         </PolarisProvider>
