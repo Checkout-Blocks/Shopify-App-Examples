9dd88671284ec526b3d02262315c7106fb0818eb
Gil Greenberg <gilgreenberg@gmail.com>
2022-12-11 16:31:31 -0500

    Retrieve the checkout data

--- web/pages/api/admin/convert.js
+++ web/pages/api/admin/convert.js
@@ -2,19 +2,66 @@
 
 import { verifyAuth } from "@api-lib/verify-jwt-auth";
 
+const GET_ABANDONED_CHECKOUT = `
+    query($id: ID!) {
+        node(id: $id) {
+            id
+            ...on AbandonedCheckout {
+                id
+                abandonedCheckoutUrl
+
+            }
+        }
+    }
+`;
+
 export default async function handler(req, res) {
     const auth = await verifyAuth(req, res);
-    const { session, shop } = auth;
 
-    console.log("shop", shop);
+    try {
+        if (req.method !== "GET") {
+            throw `request method now allowed`;
+        }
 
-    // 1. Retrieve abandoned checkout with id
+        const { shop, client } = auth;
+        const { id } = req.query;
 
-    // 2. Create payload for draft order based on abandoned checkout id
+        if (!id) {
+            throw `Missing required id query param`;
+        }
 
-    // 3. Craft new draft order
+        // Retrieve abandoned checkout with id
+        // const checkout = await client.graphql.query({
+        //     data: {
+        //         query: GET_ABANDONED_CHECKOUT,
+        //         variables: {
+        //             id: `gid://shopify/AbandonedCheckout/${id}`
+        //         },
+        //     },
+        // });
 
-    // 4. Return id of draft order
+        // Hat tip to Marc Baumbach at Seguno for this apprach
+        const checkouts = await client.rest.get({
+            path: `checkouts.json`,
+            query: {
+                since_id: id - 1,
+                limit: 1,
+            },
+        });
+        const checkout = checkouts?.body?.checkouts[0];
 
-    res.status(200).json({ success: true, id: null });
+        // Create payload for draft order based on abandoned checkout id
+        const draftOrderPayload = {};
+
+        // 3. Create new draft order
+
+        // Return id of draft order
+        const draftOrderId = null; // TODO:
+
+        res.status(200).json({ success: true, id: draftOrderId });
+    } catch (error) {
+        console.warn(error);
+        console.warn(error?.response?.errors);
+        res.status(500).json();
+    }
 }
