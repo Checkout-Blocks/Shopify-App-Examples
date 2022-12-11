521e46787c3776d736486f7908a0b383ef29231d
Gil Greenberg <gilgreenberg@gmail.com>
2022-12-11 18:49:50 -0500

    Update page logic to handle different states

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,18 +1,26 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { Button, Card, Layout, Page, Spinner } from "@shopify/polaris";
+import { Banner, Button, Card, Layout, Page, Spinner } from "@shopify/polaris";
 import Head from "next/head";
 
 import { userLoggedInFetch } from "@lib/fetch";
 import { useAppBridge } from "@shopify/app-bridge-react";
 import { Redirect } from "@shopify/app-bridge/actions";
 
+const states = {
+    idle: "idle",
+    loading: "loading",
+    error: "error",
+    redirecting: "redirecting"
+}
+
 export default function Home() {
     const router = useRouter();
     const app = useAppBridge();
     const fetchFunction = userLoggedInFetch();
 
-    const [loading, setLoading] = useState(router.query?.id ? true : false);
+    const [state, setState] = useState(states.idle);
+    const [toast, setToast] = useState(null);
 
     useEffect(() => {
         if (router.query?.id) {
@@ -22,6 +30,14 @@ export default function Home() {
 
     const convertToDraft = async () => {
         try {
+            if (state === states.loading) {
+                // Quick if already converting
+                return;
+            }
+
+            setState(states.loading);
+
+            // Send API request to convert
             const response = await fetchFunction(`/api/admin/convert?id=${router.query?.id}`).then((res) => {
                 if (!res) {
                     return null;
@@ -29,20 +45,34 @@ export default function Home() {
                 return res?.json()
             });
 
-            console.log({ response })
+            // Error out if no draft order id was returned
+            if (!response?.id) {
+                setToast({
+                    error: true,
+                })
+                setState(states.error);
+                return;
+            }
+
+            setState(states.redirecting);
+
+            // Redirect to newly created draft order
+            const redirect = Redirect.create(app);
+            redirect.dispatch(Redirect.Action.ADMIN_PATH, `/draft_orders/${response?.id}`);
             
         } catch (error) {
             console.warn(error);
         }
     }
 
+    // Helper function to redirect to abandoned checkouts
     const navigateToAbandonedCheckouts = () => {
         const redirect = Redirect.create(app);
         redirect.dispatch(Redirect.Action.ADMIN_PATH, "/checkouts");
     }
 
     if (router.query?.id) {
-        if (loading) {
+        if (state === states.loading) {
             return (
                 <Page>
                     <Layout>
@@ -57,18 +87,43 @@ export default function Home() {
                     </Layout>
                 </Page>
             )
+        } else if (state === states.error) {
+            return (
+                <Page>
+                    <Layout>
+                        <Layout.Section>
+                            <Card
+                                sectioned
+                                title="Creating draft order..."
+                            >
+                                <Banner status="critical">{toast?.message}</Banner>
+                            </Card>
+                        </Layout.Section>
+                    </Layout>
+                </Page>
+            )
+        } else if (state === states.redirecting) {
+            return (
+                <Page>
+                    <Layout>
+                        <Layout.Section>
+                            <Card
+                                sectioned
+                                title="Redirecting to draft order..."
+                            >
+                                <Spinner />
+                            </Card>
+                        </Layout.Section>
+                    </Layout>
+                </Page>
+            )
         }
 
         return (
             <Page>
                 <Layout>
                     <Layout.Section>
-                        <Card
-                            sectioned
-                            title="Redirecting to draft order..."
-                        >
-                            <Spinner />
-                        </Card>
+                        <Spinner />
                     </Layout.Section>
                 </Layout>
             </Page>
