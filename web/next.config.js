88d5a8333d8f20d7db2744977a08b85f101eae2b
Gil Greenberg <gilgreenberg@gmail.com>
2022-12-11 14:38:18 -0500

    Add security headers for loginc to prevent iframe

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+const headers = {
+    preventIframe: {
+        key: "Content-Security-Policy",
+        value: `frame-ancestors 'none';`,
+    },
+};
+
 const nextConfig = {
     reactStrictMode: true,
+    experimental: {
+        allowMiddlewareResponseBody: true,
+    },
+    async headers() {
+        return [
+            {
+                source: "/login",
+                headers: [headers.preventIframe],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
