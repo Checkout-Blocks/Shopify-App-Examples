0a6ac000758c42b8c5d24e872b89e1bbf400fce5
Gil Greenberg <3484527+gil--@users.noreply.github.com>
2023-01-23 13:41:41 -0500

    Update auth url to work with admin.shopify.com origin stores

--- checkout-to-draft-with-upstash-sessions/web/lib/fetch.js
+++ checkout-to-draft-with-upstash-sessions/web/lib/fetch.js
@@ -8,14 +8,13 @@ export function userLoggedInFetch() {
 
     return async (uri, options) => {
         const response = await fetchFunction(uri, options);
+        const urlSearchParams = new URLSearchParams(window.location.search);
+        const params = Object.fromEntries(urlSearchParams.entries());
+        const shop = params?.shop || "";
 
         if (response.status === 401) {
             const reauthUrl = response.headers["X-Shopify-API-Request-Failure-Reauthorize-Url"] || null;
-
-            const authUrl = reauthUrl || `${
-                window?.location?.origin
-            }/api/auth?shop=${app.hostOrigin?.replace("https://", "")}`;
-
+            const authUrl = reauthUrl || `${process.env.NEXT_PUBLIC_HOST}/api/auth?shop=${shop}`;
             const redirect = Redirect.create(app);
             redirect.dispatch(Redirect.Action.REMOTE, authUrl);
             
