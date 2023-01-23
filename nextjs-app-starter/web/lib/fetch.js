ff3af20190462ab0af100f4c1e02dd52f6e27bee
Gil Greenberg <3484527+gil--@users.noreply.github.com>
2023-01-23 13:43:01 -0500

    Update auth url to work with admin.shopify.com origin stores

--- nextjs-app-starter/web/lib/fetch.js
+++ nextjs-app-starter/web/lib/fetch.js
@@ -3,7 +3,10 @@ import { authenticatedFetch } from "@shopify/app-bridge-utils";
 
 export function userLoggedInFetch(app) {
     const fetchFunction = authenticatedFetch(app);
-  
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+    const shop = params?.shop || "";
+    
     return async (uri, options) => {
         const response = await fetchFunction(uri, options);
     
@@ -11,8 +14,7 @@ export function userLoggedInFetch(app) {
             response.status === 401
             //response.headers.get("X-Shopify-API-Request-Failure-Reauthorize") === "1"
         ) {         
-            const authUrl = `${process.env.NEXT_PUBLIC_HOST}/api/auth?shop=${app.hostOrigin?.replace("https://", "")}`
-            
+            const authUrl = `${process.env.NEXT_PUBLIC_HOST}/api/auth?shop=${shop}`
             const redirect = Redirect.create(app);
             redirect.dispatch(
                 Redirect.Action.REMOTE,
